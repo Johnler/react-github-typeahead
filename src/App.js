9e72182300b8b7ea5b2f75bccdb8742881ed15a7
Johnler <johnlerwonggamboa@gmail.com>
2021-02-10 16:22:20 +0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import TypeAhead from './typeahead';
-import useSWR from 'swr';
-import axios from 'axios';
+
 
 
 class App extends React.Component {
--- src/typeahead.js
+++ src/typeahead.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-function TypeAhead(props) {
+function TypeAhead() {
   const [state, setState] = useState({
     input: '',
     isShow: true,
